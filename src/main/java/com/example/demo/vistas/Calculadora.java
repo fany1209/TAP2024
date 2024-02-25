ad2e4260dea5fc36b9798988f14e4c63c195ff19
stefany <20031547@itcelaya.edu.mx>
2024-02-25 16:42:09 -0600

    -a

--- src/main/java/com/example/demo/vistas/Calculadora.java
+++ src/main/java/com/example/demo/vistas/Calculadora.java
@@ -1,73 +1,156 @@
 package com.example.demo.vistas;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import static javafx.application.Application.launch;
+
 public class Calculadora extends Stage {
+    private GridPane gridPane;
+    private TextField displayField;
+    private final Button[][] botones = new Button[4][4];
+    private Button botonLimpiar;
+    private final char[] etiquetas = {'7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '=', '+'};
 
-    private Scene escena;
-    private VBox vContenedor;
-    private GridPane gdpTeclado;
-    private TextField txtPantalla;
-    private Button[][] arBotones = new Button[4][4];  // reservo 16 lugares mas no los instancio
 
-    private char [] arEtiquetas = {'7','8','9','/','4','5','6','*','1','2','3','-','0','=','+','.'};
-   public Calculadora(){
-       CrearUI();
-       this.setTitle("mi primer calculadora");
-       this.setScene(escena);
-       this.show();
-   }
-
-    private void CrearUI() {
-       txtPantalla= new TextField("0");
-       gdpTeclado =new GridPane();
-       CrearTeclado();
-        vContenedor = new VBox(txtPantalla,gdpTeclado);
-        vContenedor.setSpacing(5);
-        escena = new Scene(vContenedor,200,300);
-        escena.getStylesheets()
-                .add(getClass().getResource("/estilos/calculadora.css").toString());
+    public Calculadora() {
+        crearUI();
+        Scene scene = new Scene(gridPane, 200, 250);
+        scene.getStylesheets().add(getClass().getResource("/estilos/calculadora.css").toString());
+        this.setTitle("Calculadora");
+        this.setScene(scene);
+        this.show();
     }
 
-    private void CrearTeclado() {
-
-       int pos = 0;
-       char simbolo;
-       for(int i = 0; i<4 ; i++){
-           for(int j =0; j < 4; j++){
-               arBotones[i][j]= new Button(arEtiquetas[pos]+"");
-               arBotones[i][j].setPrefSize(50,50);
-               int finalPos = pos;
-               arBotones[i][j].setOnAction(event -> setValue(arEtiquetas[finalPos]));
-               gdpTeclado.add(arBotones[i][j],j,i);
-
-               if(arEtiquetas[pos] == '+' || arEtiquetas[pos] == '-' || arEtiquetas[pos] == '*' || arEtiquetas[pos] == '/'  || arEtiquetas[pos] == '.'  )
-                   arBotones[i][j].setId("color-operador");
-               pos++;
-           }
-       }
-        Button btnLimpiar = new Button("C");
-        btnLimpiar.setPrefSize(50, 50);
-        btnLimpiar.setOnAction(event -> clearDisplay());
-        gdpTeclado.add(btnLimpiar, 3, 4); // Agrega el botón en la posición deseada
-
-
-    }
-    private void setValue(char simbolo) {
-        if(txtPantalla.getText().equals("0")) {
-            txtPantalla.setText("");
+    private void crearUI() {
+        gridPane = new GridPane();
+        displayField = new TextField();
+        displayField.setEditable(false);
+        displayField.setPrefColumnCount(4);
+        gridPane.add(displayField, 0, 0, 4, 1);
+        botonLimpiar = new Button("C");
+        botonLimpiar.setId("botonLimpiar");
+        botonLimpiar.setPrefSize(100, 50);
+        botonLimpiar.setOnAction((event) -> clearDisplay());
+        gridPane.add(botonLimpiar, 0, 5, 2, 1);
+        int posicion = 0;
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                final char simbolo = etiquetas[posicion];
+                botones[i - 1][j] = new Button(String.valueOf(simbolo));
+                botones[i - 1][j].setPrefSize(50, 50);
+                botones[i - 1][j].setOnAction((event) -> handleButtonClick(String.valueOf(simbolo)));
+                // Asignar clase CSS "operador" a los botones que representan operadores matemáticos
+                if (esOperador(simbolo)) {
+                    botones[i - 1][j].getStyleClass().add("operador");
+                }
+                gridPane.add(botones[i - 1][j], j, i);
+                posicion++;
+            }
         }
-        txtPantalla.appendText(simbolo + "");
+    }
+    private boolean esOperador(char caracter) {
+        return caracter == '+' || caracter == '-' || caracter == '*' || caracter == '/';
+    }
+
+
+    private void handleButtonClick(String text) {
+        switch (text) {
+            case "=":
+                evaluateExpression();
+                break;
+            default:
+                appendToDisplay(text);
+                break;
+        }
+    }
+
+    private void evaluateExpression() {
+        try {
+            String result = String.valueOf(eval(displayField.getText()));
+            displayField.setText(result);
+        } catch (Exception e) {
+            displayField.setText("Error");
+        }
     }
 
     private void clearDisplay() {
-        txtPantalla.setText("0");
+        displayField.clear();
     }
 
+    private void appendToDisplay(String text) {
+        displayField.appendText(text);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private double eval(String expression) {
+        return new Object() {
+            int pos = -1, ch;
+
+            void nextChar() {
+                ch = (++pos < expression.length()) ? expression.charAt(pos) : -1;
+            }
+
+            boolean eat(int charToEat) {
+                while (ch == ' ') nextChar();
+                if (ch == charToEat) {
+                    nextChar();
+                    return true;
+                }
+                return false;
+            }
+
+            double parse() {
+                nextChar();
+                double x = parseExpression();
+                if (pos < expression.length()) throw new RuntimeException("Carácter inesperado: " + (char) ch);
+                return x;
+            }
+
+
+            double parseExpression() {
+                double x = parseTerm();
+                for (; ; ) {
+                    if (eat('+')) x += parseTerm(); // Suma
+                    else if (eat('-')) x -= parseTerm(); // Resta
+                    else return x;
+                }
+            }
+
+
+            double parseTerm() {
+                double x = parseFactor();
+                for (; ; ) {
+                    if (eat('*')) x *= parseFactor(); // Multiplicación
+                    else if (eat('/')) x /= parseFactor(); // División
+                    else return x;
+                }
+            }
+
+            double parseFactor() {
+                if (eat('+')) return parseFactor();
+                if (eat('-')) return -parseFactor();
+                double x;
+                int startPos = this.pos;
+                if (eat('(')) { // Parentesis
+                    x = parseExpression();
+                    eat(')');
+                } else if ((ch >= '0' && ch <= '9') || ch == '.') {
+                    while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                    x = Double.parseDouble(expression.substring(startPos, this.pos));
+                } else {
+                    throw new RuntimeException("Carácter inesperado: " + (char) ch);
+                }
+
+                return x;
+            }
+        }.parse();
+    }
 }
